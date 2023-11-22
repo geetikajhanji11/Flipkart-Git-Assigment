aba7eca4fb8f6881926eeb51dd289f7bf0a75d04
geetikajhanji11 <geetikajhanji11@gmail.com>
2023-11-22 15:18:02 +0530

    Added Demo Java Project

---
+++ DemoJavaProject/src/com/flipkart/demojavaproject/Demo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.flipkart.demojavaproject;
+
+/**
+ * 
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+	}
+
+}
